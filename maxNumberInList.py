f68155fa1f2358a8832d9b736323058881b2669e
fatinsourav <fatin151485@gmail.com>
2019-07-12 10:59:36 +0600

    bug

--- maxNumberInList.py
+++ maxNumberInList.py
@@ -1,10 +1,3 @@
-list = []
-userInput = int(input())
-for i in range(userInput):
-    temp = int(input())
-    list.append(temp)
-maximum = 0
-for j in range(len(list)):
-    if list[j]>maximum:
-        maximum = list[j]
-print(maximum)
+n = int(input())
+arr = map(int,input().split())
+
